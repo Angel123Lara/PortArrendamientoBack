a1bbf9b4afb2971c6e1132004da4e7c6c141b846
JUAN-V-GTI <juan.velasco@grupotimexico.com>
2024-04-23 10:35:36 -0600

    first commit

---
+++ src/main/java/com/gupoti/mexico/portalarrendamiento/PortArrendamientoBackApplication.java
@@ -0,0 +1,13 @@
+package com.gupoti.mexico.portalarrendamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortArrendamientoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortArrendamientoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gupoti/mexico/portalarrendamiento/PortArrendamientoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gupoti.mexico.portalarrendamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortArrendamientoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
