e53c9870a4533dca57c75ba2bedd4a5cb8e43bcf
Juan Francisco Carreon Herrera <maklauss83@gmail.com>
2024-04-25 10:40:23 -0600

    CREACION DELMETODO POST PARA CONCEPT

---
+++ src/main/java/com/gupoti/mexico/portalarrendamiento/Model/Catalogos/ConceptModel.java
@@ -0,0 +1,38 @@
+package com.gupoti.mexico.portalarrendamiento.Model.Catalogos;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "XXPAR_AGR_CONCEPTS_CATALOG_T")
+public class ConceptModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="portal_concept")
+    private String portal_concept;
+    @Column(name="EBS_concept")
+    private String EBS_concept;
+    @Column(name="inactivation_date")
+    private Date inactivation_date;
+    @Column(name="creation_date")
+    private Date creation_date;
+    @Column(name="created_by")
+    private Date created_by;
+    @Column(name="last_update_date")
+    private Date last_update_date;
+    @Column(name="last_update_by")
+    private Long last_updated_by;
+    @Column(name="last_update_login")
+    private Long last_update_login;
+    // @Column(name="enable")
+    // private boolean enable;
+
+}
