ee381f593aa518cfceb38ce441e1240d0798134a
Juan Francisco Carreon Herrera <maklauss83@gmail.com>
2024-04-25 16:42:53 -0600

    CAMBIO EN VALIDACIONES

--- src/main/java/com/gupoti/mexico/portalarrendamiento/Dto/Catalogos/ConceptRequestDTO.java
+++ src/main/java/com/gupoti/mexico/portalarrendamiento/Dto/Catalogos/ConceptRequestDTO.java
@@ -14,6 +14,7 @@ public class ConceptRequestDTO{
     @NotEmpty(message="this fiels isnt't empty")
     @NotBlank(message="this fiels isnt't blanck")
     @Size(min =1, max= 30, message="the max length is 30")
+    
     private String portal_concept;
 
     @NotNull(message="this fiels isnt't null")
